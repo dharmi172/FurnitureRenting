b3e7bcd5b3d63ef9a628a8fcb24bd62b9c70f5c5
dharmi172 <140879075+dharmi172@users.noreply.github.com>
2024-06-29 14:45:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FurnitureRenting</title>
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- styling css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- navbar -->
+<nav class="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://images.freekaamaal.com/post_images/1682081199.webp" alt="" width="150" height="70" class="d-inline-block align-items-center">
+      <i class="bi bi-person-circle" id="userLogo"></i>
+      <i class="bi bi-search" id="searchBar"></i>
+      <i class="bi bi-bag-dash" id="cartBag"></i>
+    </a>
+  </div>
+</nav>
+
+    <!-- main page -->
+    <div class="container">
+        <img src="images/home.png" class="homePage">
+    </div>
+
+    <!-- chat button -->
+    <button type="button" class="btn message-us-button" id="messageUsButton" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    <a href="" class="text-decoration-none text-white"><i class="bi bi-chat-left-dots"></i>Message</a>
+    </button>
+    <!-- modal -->
+    
+
+    <!-- footer -->
+    <div class="container bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2">Furtinure</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, debitis!
+      </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h3>
+      <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+      
+      <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+      <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+      <a href="" class="d-inline-block mb-2 text-dark text-decoration-none">About</a><br>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Follow Us</h5>
+      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-twitter-x"></i> Twitter
+      </a>
+      <br>
+      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+        <i class="bi bi-instagram"></i> Instagram
+      </a>
+      <br>
+     <a href="#" class="d-inline-block text-dark text-decoration-none">
+      <i class="bi bi-facebook"></i> Facebook
+      </a>
+
+    </div>
+  </div>
+ </div>
+
+<div class="text-center p-3" id="footer"> 
+  Designed and Developed by XYZ
+</div> 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+
+<!-- bootstrap javascript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- <script src="script.js"></script> -->
+
+
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Open a chat window or link to a contact form
+document.getElementById('messageUsButton').addEventListener('click', function() {
+    // Replace with your desired action
+    alert('You clicked Message Us!');
+    // Example: Open a chat window or link to a contact form
+  });
+  
